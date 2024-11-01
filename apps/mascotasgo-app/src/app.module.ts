97d265753850ce8955fbb316e986c2a9e2b7d01d
cristopher <cristophercc67@gmail.com>
2024-11-01 19:52:57 -0300

    variables de entorno para el archivo docker compose

--- apps/mascotasgo-app/src/app.module.ts
+++ apps/mascotasgo-app/src/app.module.ts
@@ -5,9 +5,19 @@ import { UserService } from './user/user.service';
 import { PrismaService } from './prisma.service';
 import { PassportModule } from '@nestjs/passport';
 import { LoginModule } from './login/loginModule';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [userModule, LoginModule, PassportModule, JwtModule],
+  imports: [
+    userModule,
+    LoginModule, 
+    PassportModule,
+    JwtModule,
+    ConfigModule.forRoot({
+      envFilePath: '../env/development.env',
+      isGlobal: true
+    })
+  ],
   providers: [UserService, JwtService, PrismaService]
 
 })
--- apps/mascotasgo-app/src/user/user.module.ts
+++ apps/mascotasgo-app/src/user/user.module.ts
@@ -2,8 +2,10 @@ import { Module } from "@nestjs/common";
 import { userController } from "./user.controller";
 import { UserService } from "./user.service";
 import { PrismaService } from "../prisma.service";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
+    
     controllers: [userController],
     providers: [UserService, PrismaService]
 })
