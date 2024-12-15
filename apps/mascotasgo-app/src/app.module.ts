a08ba53495f9239069b1cd12648e9574cb04e950
cristopher <cristophercc67@gmail.com>
2024-12-14 23:27:50 -0300

    cambios generales añadido welcome page

--- apps/mascotasgo-app/src/app.module.ts
+++ apps/mascotasgo-app/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { userModule } from './user/user.module';
 import { UserService } from './user/user.service';
 import { PrismaService } from './prisma.service';
+import { welcomeController } from './welcome.controller';
 
 @Module({
   imports: [
     userModule,
   ],
-  providers: [UserService, PrismaService]
+  providers: [UserService, PrismaService],
+  controllers: [welcomeController]
 
 })
 export class AppModule {}
---
+++ apps/mascotasgo-app/src/welcome.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Delete, Get, Param, Post, Put, Res, UseGuards } from "@nestjs/common";
+import { Response } from "express";
+
+@Controller()
+export class welcomeController {
+
+    @Get()
+    welcome(@Res() response:Response) {
+        return response.json({message:"Bienvenido al servicio de usuarios"})
+    }
+
+}
